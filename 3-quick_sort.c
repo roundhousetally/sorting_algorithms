37675cfdc30dbe080d5dbb22f80578fba2d71942
roundhousetally <tahlia.w.roper@gmail.com>
2020-06-15 15:22:54 +0000

    task 3 setup

---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of ints using quicksort algo
+ * @array: the array to sort
+ * @size: size of the array
+ * Return: Void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+}
