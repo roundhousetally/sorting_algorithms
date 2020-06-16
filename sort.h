409d6af932a0ebb13db29eedb256d6b7a3fb74b6
Corbin Enterline <corbinenterline1@gmail.com>
2020-06-16 09:42:28 +0000

    Header, whoopsie

--- sort.h
+++ sort.h
@@ -27,5 +27,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int *left, int *right);
+void swap_node(listint_t *list, listint_t *left, listint_t *right);
 #endif
